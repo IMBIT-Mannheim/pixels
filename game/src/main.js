116168bbe35375222a7f082e8195ec1d27392d80
Paul Charwat <paul.charwat@icloud.com>
2024-10-31 12:34:01 +0100

    feat: add keyboard movement controls for player character in lvl 2

--- game/src/main.js
+++ game/src/main.js
@@ -479,6 +479,59 @@ k.scene("test", async () => {
 	}
 
 	k.onMouseRelease(stopAnims);
+
+	k.onKeyRelease(() => {
+		stopAnims();
+	});
+	k.onKeyDown((key) => {
+		const keyMap = [
+			k.isKeyDown("right"),
+			k.isKeyDown("left"),
+			k.isKeyDown("up"),
+			k.isKeyDown("down"),
+		];
+
+		let nbOfKeyPressed = 0;
+		for (const key of keyMap) {
+			if (key) {
+				nbOfKeyPressed++;
+			}
+		}
+
+		if (nbOfKeyPressed > 1) return;
+
+		if (player.isInDialogue) return;
+
+		//Player keyboard movement
+		if (keyMap[0]) {
+			player.flipX = false;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "right";
+			player.move(player.speed, 0);
+			return;
+		}
+
+		if (keyMap[1]) {
+			player.flipX = true;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "left";
+			player.move(-player.speed, 0);
+			return;
+		}
+
+		if (keyMap[2]) {
+			if (player.getCurAnim().name !== "walk-up") player.play("walk-up");
+			player.direction = "up";
+			player.move(0, -player.speed);
+			return;
+		}
+
+		if (keyMap[3]) {
+			if (player.getCurAnim().name !== "walk-down") player.play("walk-down");
+			player.direction = "down";
+			player.move(0, player.speed);
+		}
+	});
 });
 
 k.go("main");
