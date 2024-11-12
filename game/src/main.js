862b0bdd7295a38938723866745b5c14347b072e
Paul Charwat <paul.charwat@icloud.com>
2024-11-12 12:29:33 +0100

    added bg music

--- game/src/main.js
+++ game/src/main.js
@@ -34,6 +34,23 @@ k.loadSprite("map", "./map.png");
 //setzt die Hintergrundfarbe
 k.setBackground(k.Color.fromHex("#311047"));
 
+//sounds
+k.loadMusic("bg-music", "./bg-music.mp3");
+
+k.scene("loading", () => {
+	k.add([
+		k.text("Press enter to beginn...", 32),
+		k.pos(k.width() / 2, k.height() / 2),
+	]);
+	k.onKeyDown("enter", () => {
+		const musik = k.play("bg-music", {});
+		musik.volume = 1;
+		musik.loop = true;
+
+		k.go("main");
+	});
+});
+
 //LVL 1: SCENE MAIN 
 k.scene("main", async () => {
 	let isFullMapView = false;  // Variable to track if in full map view
@@ -164,246 +181,246 @@ k.scene("main", async () => {
 	k.onUpdate(() => {
 		k.camPos(player.worldPos().x, player.worldPos().y - 100);
 	});
-		// Show full world map while holding down m key
-		k.onKeyDown("m", () => {
-			isFullMapView = true;
-			enableFullMapView(k, map);
-		});
-		// Return to player view when releasing m key
-		k.onKeyRelease("m", () => {
-			isFullMapView = false;
-			disableFullMapView(k);
-		});
-
-		showWorldMapBtn.addEventListener("click", () => {
-			if (!isFullMapView) {
-				isFullMapView = true;
-				showWorldMapBtn.innerHTML = "Hide World Map (M)";
-				enableFullMapView(k, map);
-			} else {
-				isFullMapView = false;
-				showWorldMapBtn.innerHTML = "Show World Map (M)";
-				document.getElementById("game").focus();
-				disableFullMapView(k);
-			}
-		});
-
-		k.onUpdate(() => {
-			if (!isFullMapView) {
-				// Follow the player only if not in full map view
-				k.camPos(player.worldPos().x, player.worldPos().y - 100);
-			}
-		});
-
-		//Fügt die Collider hinzu und prüft, ob der collider einen Namen hat. Wenn ja, wird ein Dialog angezeigt. Der dialog wird in der Datei constants.js definiert.
-		for (const layer of layers) {
-			if (layer.name === "boundaries") {
-				for (const boundary of layer.objects) {
-					map.add([
-						k.area({
-							shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
-						}),
-						k.body({ isStatic: true }),
-						k.pos(boundary.x, boundary.y),
-						boundary.name,
-					]);
-
-					if (boundary.name) {
-						player.onCollide(boundary.name, () => {
-							player.isInDialogue = true;
-							displayDialogue(
-								dialogueData[boundary.name],
-								() => (player.isInDialogue = false)
-							);
-						});
-					}
-				}
-
-				continue;
-			}
-
-			if (layer.name === "goto") {
-				for (const boundary of layer.objects) {
-					map.add([
-						k.area({
-							shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
-						}),
-						k.body({ isStatic: true }),
-						k.pos(boundary.x, boundary.y),
-						boundary.name,
-					]);
-
-					if (boundary.name) {
-						player.onCollide(boundary.name, () => {
-							k.go(boundary.name);
-						});
-					}
-				}
-				continue;
-			}
-
-			//Setzt den Spieler auf die Spawnposition
-			if (layer.name === "spawnpoints") {
-				for (const entity of layer.objects) {
-					if (entity.name === "player") {
-						player.pos = k.vec2(
-							(map.pos.x + entity.x) * scaleFactor,
-							(map.pos.y + entity.y) * scaleFactor
-						);
-						k.add(player);
-					}
-					else if (entity.name === "dog") {
-						dog.pos = k.vec2(
-							(map.pos.x + entity.x) * scaleFactor,
-							(map.pos.y + entity.y) * scaleFactor
-						);
-						k.add(dog);
-					}
-				}
-			}
-		}
-
-		setCamScale(k);
-
-		k.onResize(() => {
-			setCamScale(k);
-		});
-
-		//Bewegung des Spielers mit der Maus
-		k.onMouseDown((mouseBtn) => {
-			if (isFullMapView) return; // Disable player movement when in full map view
-			if (mouseBtn !== "left" || player.isInDialogue) return;
-
-			const worldMousePos = k.toWorld(k.mousePos());
-			player.moveTo(worldMousePos, player.speed);
-
-			const mouseAngle = player.pos.angle(worldMousePos);
-
-			const lowerBound = 50;
-			const upperBound = 125;
-
-			if (
-				mouseAngle > lowerBound &&
-				mouseAngle < upperBound &&
-				player.getCurAnim().name !== "walk-up"
-			) {
-				player.play("walk-up");
-				player.direction = "up";
-				return;
-			}
-
-			if (
-				mouseAngle < -lowerBound &&
-				mouseAngle > -upperBound &&
-				player.getCurAnim().name !== "walk-down"
-			) {
-				player.play("walk-down");
-				player.direction = "down";
-				return;
-			}
-
-			if (Math.abs(mouseAngle) > upperBound) {
-				player.flipX = true;
-				if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
-				player.direction = "left";
-				return;
-			}
-
-			if (Math.abs(mouseAngle) < lowerBound) {
-				player.flipX = false;
-				if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
-				player.direction = "left";
-
-			}
-		});
-
-		function stopAnims() {
-			if (player.direction === "down") {
-				player.play("idle-down");
-				return;
-			}
-			if (player.direction === "up") {
-				player.play("idle-up");
-				return;
-			}
-
-			player.play("idle-side");
-		}
-
-		function stopDogAnims() {
-			if (dog.direction === "down") {
-				dog.play("dog-idle-down");
-				return;
-			}
-			if (dog.direction === "up") {
-				dog.play("dog-idle-up");
-				return;
-			}
-
-			dog.play("dog-idle-side");
-		}
-
-		k.onMouseRelease(stopAnims);
-
-		k.onKeyRelease(() => {
-			stopAnims();
-			stopDogAnims();
-		});
-
-		k.onKeyDown(() => {
-			if (isFullMapView) return; // Disable player movement when in full map view
-			const keyMap = [
-				k.isKeyDown("right"),
-				k.isKeyDown("left"),
-				k.isKeyDown("up"),
-				k.isKeyDown("down"),
-				k.isKeyDown("w"),
-				k.isKeyDown("a"),
-				k.isKeyDown("s"),
-				k.isKeyDown("d"),
-			];
-
-			let nbOfKeyPressed = 0;
-			for (const key of keyMap) {
-				if (key) {
-					nbOfKeyPressed++;
-				}
-			}
-
-			if (nbOfKeyPressed > 1) return;
-
-			if (player.isInDialogue) return;
-
-			//Player keyboard movement
-			if (keyMap[0] || keyMap[7]) {
-				player.flipX = true;
-				if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
-				player.direction = "right";
-				player.move(player.speed, 0);
-				return;
-			}
-
-			if (keyMap[1] || keyMap[5]) {
-				player.flipX = false;
-				if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
-				player.direction = "left";
-				player.move(-player.speed, 0);
-				return;
-			}
-
-			if (keyMap[2] || keyMap[4]) {
-				if (player.getCurAnim().name !== "walk-up") player.play("walk-up");
-				player.direction = "up";
-				player.move(0, -player.speed);
-				return;
-			}
-
-			if (keyMap[3] || keyMap[6]) {
-				if (player.getCurAnim().name !== "walk-down") player.play("walk-down");
-				player.direction = "down";
-				player.move(0, player.speed);
-			}
-		});
+	// Show full world map while holding down m key
+	k.onKeyDown("m", () => {
+		isFullMapView = true;
+		enableFullMapView(k, map);
+	});
+	// Return to player view when releasing m key
+	k.onKeyRelease("m", () => {
+		isFullMapView = false;
+		disableFullMapView(k);
 	});
 
-	k.go("main");
+	showWorldMapBtn.addEventListener("click", () => {
+		if (!isFullMapView) {
+			isFullMapView = true;
+			showWorldMapBtn.innerHTML = "Hide World Map (M)";
+			enableFullMapView(k, map);
+		} else {
+			isFullMapView = false;
+			showWorldMapBtn.innerHTML = "Show World Map (M)";
+			document.getElementById("game").focus();
+			disableFullMapView(k);
+		}
+	});
+
+	k.onUpdate(() => {
+		if (!isFullMapView) {
+			// Follow the player only if not in full map view
+			k.camPos(player.worldPos().x, player.worldPos().y - 100);
+		}
+	});
+
+	//Fügt die Collider hinzu und prüft, ob der collider einen Namen hat. Wenn ja, wird ein Dialog angezeigt. Der dialog wird in der Datei constants.js definiert.
+	for (const layer of layers) {
+		if (layer.name === "boundaries") {
+			for (const boundary of layer.objects) {
+				map.add([
+					k.area({
+						shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+					}),
+					k.body({ isStatic: true }),
+					k.pos(boundary.x, boundary.y),
+					boundary.name,
+				]);
+
+				if (boundary.name) {
+					player.onCollide(boundary.name, () => {
+						player.isInDialogue = true;
+						displayDialogue(
+							dialogueData[boundary.name],
+							() => (player.isInDialogue = false)
+						);
+					});
+				}
+			}
+
+			continue;
+		}
+
+		if (layer.name === "goto") {
+			for (const boundary of layer.objects) {
+				map.add([
+					k.area({
+						shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+					}),
+					k.body({ isStatic: true }),
+					k.pos(boundary.x, boundary.y),
+					boundary.name,
+				]);
+
+				if (boundary.name) {
+					player.onCollide(boundary.name, () => {
+						k.go(boundary.name);
+					});
+				}
+			}
+			continue;
+		}
+
+		//Setzt den Spieler auf die Spawnposition
+		if (layer.name === "spawnpoints") {
+			for (const entity of layer.objects) {
+				if (entity.name === "player") {
+					player.pos = k.vec2(
+						(map.pos.x + entity.x) * scaleFactor,
+						(map.pos.y + entity.y) * scaleFactor
+					);
+					k.add(player);
+				}
+				else if (entity.name === "dog") {
+					dog.pos = k.vec2(
+						(map.pos.x + entity.x) * scaleFactor,
+						(map.pos.y + entity.y) * scaleFactor
+					);
+					k.add(dog);
+				}
+			}
+		}
+	}
+
+	setCamScale(k);
+
+	k.onResize(() => {
+		setCamScale(k);
+	});
+
+	//Bewegung des Spielers mit der Maus
+	k.onMouseDown((mouseBtn) => {
+		if (isFullMapView) return; // Disable player movement when in full map view
+		if (mouseBtn !== "left" || player.isInDialogue) return;
+
+		const worldMousePos = k.toWorld(k.mousePos());
+		player.moveTo(worldMousePos, player.speed);
+
+		const mouseAngle = player.pos.angle(worldMousePos);
+
+		const lowerBound = 50;
+		const upperBound = 125;
+
+		if (
+			mouseAngle > lowerBound &&
+			mouseAngle < upperBound &&
+			player.getCurAnim().name !== "walk-up"
+		) {
+			player.play("walk-up");
+			player.direction = "up";
+			return;
+		}
+
+		if (
+			mouseAngle < -lowerBound &&
+			mouseAngle > -upperBound &&
+			player.getCurAnim().name !== "walk-down"
+		) {
+			player.play("walk-down");
+			player.direction = "down";
+			return;
+		}
+
+		if (Math.abs(mouseAngle) > upperBound) {
+			player.flipX = true;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "left";
+			return;
+		}
+
+		if (Math.abs(mouseAngle) < lowerBound) {
+			player.flipX = false;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "left";
+
+		}
+	});
+
+	function stopAnims() {
+		if (player.direction === "down") {
+			player.play("idle-down");
+			return;
+		}
+		if (player.direction === "up") {
+			player.play("idle-up");
+			return;
+		}
+
+		player.play("idle-side");
+	}
+
+	function stopDogAnims() {
+		if (dog.direction === "down") {
+			dog.play("dog-idle-down");
+			return;
+		}
+		if (dog.direction === "up") {
+			dog.play("dog-idle-up");
+			return;
+		}
+
+		dog.play("dog-idle-side");
+	}
+
+	k.onMouseRelease(stopAnims);
+
+	k.onKeyRelease(() => {
+		stopAnims();
+		stopDogAnims();
+	});
+
+	k.onKeyDown(() => {
+		if (isFullMapView) return; // Disable player movement when in full map view
+		const keyMap = [
+			k.isKeyDown("right"),
+			k.isKeyDown("left"),
+			k.isKeyDown("up"),
+			k.isKeyDown("down"),
+			k.isKeyDown("w"),
+			k.isKeyDown("a"),
+			k.isKeyDown("s"),
+			k.isKeyDown("d"),
+		];
+
+		let nbOfKeyPressed = 0;
+		for (const key of keyMap) {
+			if (key) {
+				nbOfKeyPressed++;
+			}
+		}
+
+		if (nbOfKeyPressed > 1) return;
+
+		if (player.isInDialogue) return;
+
+		//Player keyboard movement
+		if (keyMap[0] || keyMap[7]) {
+			player.flipX = true;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "right";
+			player.move(player.speed, 0);
+			return;
+		}
+
+		if (keyMap[1] || keyMap[5]) {
+			player.flipX = false;
+			if (player.getCurAnim().name !== "walk-side") player.play("walk-side");
+			player.direction = "left";
+			player.move(-player.speed, 0);
+			return;
+		}
+
+		if (keyMap[2] || keyMap[4]) {
+			if (player.getCurAnim().name !== "walk-up") player.play("walk-up");
+			player.direction = "up";
+			player.move(0, -player.speed);
+			return;
+		}
+
+		if (keyMap[3] || keyMap[6]) {
+			if (player.getCurAnim().name !== "walk-down") player.play("walk-down");
+			player.direction = "down";
+			player.move(0, player.speed);
+		}
+	});
+});
+
+k.go("loading");
