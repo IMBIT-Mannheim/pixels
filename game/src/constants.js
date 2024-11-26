6bec51a7beb405aabb9f1c588d71e33d5def2d15
Paul Charwat <info@paul-charwat.de>
2024-11-26 10:15:48 +0100

    added downloadable files

--- game/src/constants.js
+++ game/src/constants.js
@@ -27,19 +27,31 @@ export const dialogueData = {
   //Klassenzimmer
   factone: {
     title: '5 Fakten ueber Mannheim',
-    text: `1.Quadratestadt: Mannheim ist einzigartig in Deutschland – die Innenstadt ist als Quadrate/Raster aufgebaut, statt Straßennamen gibt es Buchstaben- und Zahlenkombinationen.\n2.Wasserturm: Das Wahrzeichen Mannheims, der Wasserturm, ist 60 Meter hoch und ein beliebter Treffpunkt im Stadtzentrum.\n3. Erfindung des Automobils: Carl Benz erfand in Mannheim 1885 das erste Automobil – ein weltverändernder Moment!\n4.Musikstadt: Mannheim ist eine bedeutende Musikstadt und Geburtsort der "Mannheimer Schule," einer wichtigen musikalischen Bewegung des 18. Jahrhunderts, die die klassische Musik nachhaltig prägte.\n5. Multikulturell: Mannheim ist bekannt für seine kulturelle Vielfalt und veranstaltet jedes Jahr das Multikulti-Festival "Mannheimer Stadtfest“.`,
+    text: `1.Quadratestadt: Mannheim ist einzigartig in Deutschland – die Innenstadt ist als Quadrate/Raster aufgebaut, statt Strassennamen gibt es Buchstaben- und Zahlenkombinationen. \n2.Wasserturm: Das Wahrzeichen Mannheims, der Wasserturm, ist 60 Meter hoch und ein beliebter Treffpunkt im Stadtzentrum. \n3. Erfindung des Automobils: Carl Benz erfand in Mannheim 1885 das erste Automobil – ein weltveraendernder Moment! \n4.Musikstadt: Mannheim ist eine bedeutende Musikstadt und Geburtsort der "Mannheimer Schule," einer wichtigen musikalischen Bewegung des 18. Jahrhunderts, die die klassische Musik nachhaltig praegte. \n5. Multikulturell: Mannheim ist bekannt fuer seine kulturelle Vielfalt und veranstaltet jedes Jahr das Multikulti-Festival "Mannheimer Stadtfest".`,
     answers: [
     ],
   },
   facttwo: {
     title: '5 Fakten uebers Duale Studium',
-    text: `1. Praxis und Theorie: Ein duales Studium kombiniert Hochschulstudium mit praktischer Arbeit im Unternehmen.\n2. Vergütung: Studierende erhalten ein Gehalt während des gesamten Studiums.\n3. Kurze Studiendauer: Ein duales Studium dauert 3 Jahre. \n4. Hohe Übernahmechancen: Viele Unternehmen übernehmen ihre dualen Studierenden nach dem Abschluss.\n5. Wechsel alle 3 Monate: Theorie- und Praxisphasen wechseln alle drei Monate.`,
+    text: `1. Praxis und Theorie: Ein duales Studium kombiniert Hochschulstudium mit praktischer Arbeit im Unternehmen.\n2. Verguetung: Studierende erhalten ein Gehalt waehrend des gesamten Studiums.\n3. Kurze Studiendauer: Ein duales Studium dauert 3 Jahre. \n4. Hohe uebernahmechancen: Viele Unternehmen uebernehmen ihre dualen Studierenden nach dem Abschluss.\n5. Wechsel alle 3 Monate: Theorie- und Praxisphasen wechseln alle drei Monate.`,
     answers: [
     ],
   },
   factthree: {
-    title: '5 Fakten ueber die DHBW MAnnheim',
-    text: `1. Zweitgrößter Standort: Die DHBW Mannheim ist mit über 5.500 Studierenden und rund 2.000 Partnerunternehmen der zweitgrößte Standort der Dualen Hochschule Baden-Württemberg.\n2. Breites Studienangebot: Sie bietet mehr als 45 Studienrichtungen in den Fakultäten Wirtschaft und Technik an.\n3. 50-jähriges Jubiläum: Im Jahr 2024 feiert die DHBW Mannheim ihr 50-jähriges Bestehen und blickt auf eine erfolgreiche Geschichte des dualen Studiums zurück.\n4. Hohe Absolventenzahl: Im Jahr 2023 schlossen 1.542 Studierende ihr Studium erfolgreich ab.\n5. Starke Vernetzung: Mit rund 1.900 Dualen Partnern aus verschiedenen Branchen ist die DHBW Mannheim eng mit der Wirtschaft verknüpft und fördert praxisnahe Ausbildung.`,
+    title: '5 Fakten ueber die DHBW Mannheim',
+    text: `1. Zweitgroesster Standort: Die DHBW Mannheim ist mit ueber 5.500 Studierenden und rund 2.000 Partnerunternehmen der zweitgroesste Standort der Dualen Hochschule Baden-Wuerttemberg. \n2. Breites Studienangebot: Sie bietet mehr als 45 Studienrichtungen in den Fakultaeten Wirtschaft und Technik an. \n3. 50-jaehriges Jubilaeum: Im Jahr 2024 feiert die DHBW Mannheim ihr 50-jaehriges Bestehen und blickt auf eine erfolgreiche Geschichte des dualen Studiums zurueck. \n4. Hohe Absolventenzahl: Im Jahr 2023 schlossen 1.542 Studierende ihr Studium erfolgreich ab. \n5. Starke Vernetzung: Mit rund 1.900 Dualen Partnern aus verschiedenen Branchen ist die DHBW Mannheim eng mit der Wirtschaft verknuepft und foerdert praxisnahe Ausbildung. `,
+    answers: [
+    ],
+  },
+  factfour: {
+    title: 'factfour',
+    text: `factfour. `,
+    answers: [
+    ],
+  },
+  factfive: {
+    title: 'factfive',
+    text: `factfive. `,
     answers: [
     ],
   },
@@ -61,7 +73,7 @@ export const dialogueData = {
   siranq: [{
     id: 0,
     title: 'siranq',
-    text: `Das ist random text über Siranq \n Das ist die Frage zu den Antworten unten!`,
+    text: `Das ist random text ueber Siranq \n Das ist die Frage zu den Antworten unten! `,
 
     answers: [
       'Answer 1',
@@ -75,7 +87,7 @@ export const dialogueData = {
   }, {
     id: 1,
     title: 'siranq',
-    text: `ssssssssssssss`,
+    text: `sssssssssssssst `,
     answers: [
       'Answer 1',
       'Answer 2',
@@ -88,32 +100,27 @@ export const dialogueData = {
   },],
   boehringer: {
     title: 'boehringer',
-    text: `boehringer`,
+    text: `boehringer `,
     answers: []
   },
   suitguy: {
     title: 'suitguy',
-    text: `suitguy`,
-    answers: []
-  },
-  suitguy: {
-    title: 'suitguy',
-    text: `suitguy`,
+    text: `suitguy `,
     answers: []
   },
   pwc: {
     title: 'pwc',
-    text: `pwc`,
+    text: `pwc `,
     answers: []
   },
   linde: {
     title: 'linde',
-    text: `linde`,
+    text: `linde `,
     answers: []
   },
   eviden: {
     title: 'eviden',
-    text: `eviden`,
+    text: `eviden `,
     answers: []
   },
 };
